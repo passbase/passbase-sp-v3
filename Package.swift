383d7f710a7f7c3003c31c7f863cba69521389e3
Skorostetskyi Sergii <sergii@passbase.com>
2022-09-21 13:41:36 +0200

    Release 3.3.0

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.4
+
+import PackageDescription
+
+let package = Package(
+    name: "Passbase",
+    products: [
+        .library(name: "Passbase",
+                 targets: ["Passbase", "Microblink"]),
+    ],
+    targets: [
+        .binaryTarget(name: "Passbase",
+                      url: "https://button.passbase.com/__ios/Passbase_3.3.0.zip",
+                      checksum: "8541fb253a276befbe065ba2a29eea55961f9381b0fb5b40d9e1ce336153f401"),
+        .binaryTarget(name: "Microblink",
+                      url: "https://button.passbase.com/__ios/Microblink_5.16.1.zip",
+                      checksum: "33af45af2820e2bc0ff929ade3714c09c923ed03a1606d5219e16b9f87ddeeed"),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,6 @@
+# Swift Package for Passbase SDK for iOS
+
+## Installation
+
+- Add `.package(url: "https://github.com/passbase/passbase-sp.git", from: "3.3.0")` to your Package.swift file's dependencies.
+- Update your packages using `$ swift package update`.
